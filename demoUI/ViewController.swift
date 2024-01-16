fd67a7693ef5bac6c3d53add4c109b86c22339d8
prachitrivedi096 <prachiptrivedi096@gmail.com>
2024-01-15 19:55:04 -0800

    Initial Commit

---
+++ demoUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  demoUI
+//
+//  Created by user236101 on 1/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
